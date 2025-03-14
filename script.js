fe3788133bd4e835bd6487032731c3896fad79ba
Adps75 <dps.adrien@gmail.com>
2025-03-14 17:27:49 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,42 @@
 let map = L.map('map').setView([48.8566, 2.3522], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+let satellite = L.tileLayer(
+    "https://wxs.ign.fr/pratique/geoportail/wmts?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile" +
+    "&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}" +
+    "&FORMAT=image/jpeg", { attribution: "IGN - Geoportail", maxZoom: 19 });
+
+let cadastre = L.tileLayer(
+    "https://wxs.ign.fr/pratique/geoportail/wmts?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile" +
+    "&LAYER=CADASTRAL.PARCELS&STYLE=PCI&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}" +
+    "&FORMAT=image/png", { attribution: "IGN - Cadastre", maxZoom: 19, opacity: 0.7 });
+
+satellite.addTo(map);
+
+function toggleSatellite() {
+    map.addLayer(satellite);
+}
+
+function toggleCadastre() {
+    map.addLayer(cadastre);
+}
 
 let pointsEau = [];
 let zones = [];
-let drawing = false;
-let currentPolygon = [];
-
-map.on('zoomend', function() {
-    console.log("Zoom actuel :", map.getZoom());
-});
 
 function addPointEau() {
     map.on('click', function (e) {
-        let marker = L.marker([e.latlng.lat, e.latlng.lng], { draggable: true }).addTo(map);
+        let marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
         pointsEau.push({ lat: e.latlng.lat, lng: e.latlng.lng });
-
-        marker.on('dragend', function (event) {
-            let newPos = event.target.getLatLng();
-            let index = pointsEau.findIndex(p => p.lat === e.latlng.lat && p.lng === e.latlng.lng);
-            pointsEau[index] = { lat: newPos.lat, lng: newPos.lng };
-        });
-
-        map.off('click');
-    });
-}
-
-function startDrawing() {
-    drawing = true;
-    currentPolygon = [];
-    alert("Cliquez sur la carte pour dessiner une zone, double-cliquez pour terminer.");
-    
-    map.on('click', function (e) {
-        if (!drawing) return;
-        currentPolygon.push([e.latlng.lat, e.latlng.lng]);
-        L.circleMarker([e.latlng.lat, e.latlng.lng], { radius: 3, color: 'blue' }).addTo(map);
-    });
-
-    map.on('dblclick', function () {
-        if (currentPolygon.length > 2) {
-            let polygon = L.polygon(currentPolygon, { color: 'green' }).addTo(map);
-            zones.push({ coords: currentPolygon });
-        }
-        drawing = false;
         map.off('click');
     });
 }
 
 function sendData() {
-    let data = {
-        address: "Adresse Test",
-        zones: zones,
-        points_eau: pointsEau,
-        pression: 3.5,
-        zoom: map.getZoom(),
-        fill_time: 10
-    };
+    let data = { address: "Adresse Test", zones: zones, points_eau: pointsEau, pression: 3.5, zoom: map.getZoom(), fill_time: 10 };
 
     fetch("http://127.0.0.1:5000/generate_plan", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
+        method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(data)
     })
-    .then(response => response.json())
-    .then(result => console.log("Plan généré :", result))
+    .then(response => response.json()).then(result => console.log("Plan généré :", result))
     .catch(error => console.error("Erreur :", error));
 }
